b1854c230399fd8fc11cab2a03ef2c5a15359c27
Naveen-496 <naveenreddys496@gmail.com>
2023-12-08 21:44:14 -0800

    posts

---
+++ src/main/java/dev/reddy/webserviceone/domain/Post.java
@@ -0,0 +1,4 @@
+package dev.reddy.webserviceone.domain;
+
+public record Post(Integer id, String title, String body) {
+}
--- src/main/java/dev/reddy/webserviceone/home/HomeController.java
+++ src/main/java/dev/reddy/webserviceone/home/HomeController.java
@@ -1,8 +1,11 @@
 package dev.reddy.webserviceone.home;
 
+import dev.reddy.webserviceone.domain.Post;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/")
@@ -11,5 +14,13 @@ public class HomeController {
     @GetMapping()
     public String sayHello() {
         return "Hello, World";
+    }
+
+    @GetMapping("/posts")
+    public List<Post> getAllPosts() {
+        return List.of(
+                new Post(1, "Spring Boot with Docker", "Configure and build docker images with spring boot"),
+                new Post(2, "Docker Guide", "Dockerize your spring boot in a breeze")
+        );
     }
 }
